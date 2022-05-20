a27387f6386bcc14d81575ad060a7b37c91eec7e
pnb <pnigelb@gmail.com>
2022-05-20 10:20:08 -0500

    Remove leftover code from verbose option

--- main_latex.py
+++ main_latex.py
@@ -37,8 +37,6 @@ with open(mk4_template) as infile:
     with open(os.path.join(extracted_dir, 'make4ht_with_bibtex.mk4'), 'w') as ofile:
         ofile.write('Make:add("bibtex", "%s ${input}")\n' % bib_backend)
         ofile.write(infile.read())
-verbose_args = {}
-# not_verbose_args = {'stdout': subprocess.DEVNULL, 'stderr': subprocess.DEVNULL}
 retcode = subprocess.call('make4ht --output-dir .. --format html5+common_domfilters '
                           '--build-file make4ht_with_bibtex.mk4 tmp-make4ht.tex '
                           '"mathml,mathjax,svg,fn-in"', shell=True, cwd=extracted_dir)
