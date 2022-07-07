2796bdf9d14475f7f510e946138223a4861a4ec0
pnb <pnigelb@gmail.com>
2022-07-07 11:05:19 -0500

    Use new table resizing script in conversion

--- shared_utils.py
+++ shared_utils.py
@@ -329,6 +329,7 @@ def save_soup(soup: bs4.BeautifulSoup, output_filename: str) -> None:
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../edm2022.css" />
+        <script src="../table_sizer.js" defer />
         <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
         <script id="MathJax-script" async
             src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
