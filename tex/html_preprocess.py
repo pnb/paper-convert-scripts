b3697f7d7685d53ba9b07a1e246c0ccecb92af1c
Nigel Bosch <pnigelb@gmail.com>
2023-10-06 16:01:39 -0500

    Add note about reason for \mbox in cites

--- tex/html_preprocess.py
+++ tex/html_preprocess.py
@@ -1,6 +1,8 @@
 def fix_et_al(html_str: str) -> str:
     """Fix a strange "et al." issue that occurs with some papers. It is possible the
-    compilation process is slightly wrong.
+    compilation process is slightly wrong. This is related to LaTeX adding \\mbox in
+    citations that have "et al.", which also causes the compile error
+    `Command \: unavailable in encoding OT1.`
 
     Args:
         html_str (str): Make4ht output *before* being put into BeautifulSoup
