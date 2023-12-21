8c5a99796a5e85f53217f7aadc611866433b20f1
Nigel Bosch <pnigelb@gmail.com>
2023-12-21 14:40:39 -0600

    More expansive checking for et al. mbox errors

--- tex/html_preprocess.py
+++ tex/html_preprocess.py
@@ -1,3 +1,6 @@
+import re
+
+
 def fix_et_al(html_str: str) -> str:
     """Fix a strange "et al." issue that occurs with some papers. It is possible the
     compilation process is slightly wrong. This is related to LaTeX adding \\mbox in
@@ -10,13 +13,10 @@ def fix_et_al(html_str: str) -> str:
     Returns:
         str: Modified HTML string
     """
-    # First replace the version with a space after the last mbox, then without
-    return (
-        html_str.replace("\xa0almbox .<span class='accentb'>:</span>mbox ", " al.")
-        .replace("\xa0almbox .<span class='accentb'>:</span>mbox", " al.")
-        .replace(  # Replace a version that appears in footnote citations
-            "\xa0almbox .</span><span class='accentb'><span class='ptmr7t-'>:"
-            "</span></span><span class='ptmr7t-'>mbox </span><span class='ptmr7t-'>",
-            " al.",
-        )
+    # The </span> version occurs during footnote citations
+    return re.sub(
+        r"\xa0almbox \..*?mbox ?(</span><span class='ptmr7t-'>)?",
+        " al.",
+        html_str,
+        flags=re.DOTALL,  # Match \n
     )
