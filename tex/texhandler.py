17031e2649441f794adfddbc19b059087fa79349
Nigel Bosch <pnigelb@gmail.com>
2024-04-12 22:13:13 -0500

    Add a couple more bold/italic fonts

--- tex/texhandler.py
+++ tex/texhandler.py
@@ -186,8 +186,10 @@ class TeXHandler:
             "ectc-": "code",
             "pcrr7t-x-x-120": "code",
             "ptmb7t-x-x-120": "strong",
+            "ptmb7t-": "strong",
             "ptmri7t-x-x-120": "em",
             "ptmri7t-x-x-109": "em",
+            "ptmri7t-": "em",
             "aebxti-": ["strong", "em"],
             "aer-7": None,  # Unwrap; not a good/necessary style to keep (tiny text)
         }
