d1fcb52b8d298e0519c15f331ec9ed0ef0490adf
Nigel Bosch <pnigelb@gmail.com>
2025-01-13 18:29:48 -0600

    better way to check PDF page size/margins

--- pdf_checker.py
+++ pdf_checker.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-import subprocess
+
+from PIL import Image
 
 import shared
 
@@ -9,67 +10,48 @@ ap.add_argument("pdf_path", help="PDF file path")
 args = ap.parse_args()
 
 
-def get_image_size(fname):
-    try:
-        stdout = subprocess.check_output(
-            "identify -format '%w %h' " + os.path.basename(fname),
-            shell=True,
-            cwd=os.path.dirname(args.pdf_path),
-        )
-    except subprocess.CalledProcessError:
-        exit(1)
-    width, height = stdout.decode("utf-8").strip().split(" ")
-    return int(width), int(height)
+def count_nonblank_pixels(img: Image, x1: int, y1: int, x2: int, y2: int) -> int:
+    """Return the number of non-blank pixels in the given rectangle. Assumes image is
+    in grayscale via `img.convert("L")`."""
+    img = img.crop((x1, y1, x2, y2))
+    return sum(1 for pixel in img.getdata() if pixel < 255)
 
 
+curdir = os.path.dirname(args.pdf_path)
 retcode = shared.exec_grouping_subprocesses(
-    "convert -density 100 " + os.path.basename(args.pdf_path) + " -trim crop-%d.png",
+    "convert -density 100 -background white -alpha remove -alpha off "
+    + os.path.basename(args.pdf_path)
+    + " page-%d.png",
     shell=True,
-    cwd=os.path.dirname(args.pdf_path),
+    cwd=curdir,
 )
 if retcode != 0:
     exit(retcode)
 
-# Check the size of each page after cropping to see if it is out of margins
-for fname in os.listdir(os.path.dirname(args.pdf_path)):
-    if fname.startswith("crop-"):
-        width, height = get_image_size(fname)
+# Count non-blank pixels in margin to see if margins are correctly empty
+for fname in os.listdir(curdir):
+    if fname.startswith("page-"):
         page_num = int(fname.split("-")[1].split(".")[0]) + 1
-        if width > 710:
-            print(
-                "margins: Page",
-                page_num,
-                "exceeds horizontal margin; expected 7 inches of text width between "
-                "margins, found %.2f" % (width / 100),
-            )
-        if height > 935:  # Should be 11 - 1 - 0.75 = 9.25 inches
-            print(
-                "margins: Page",
-                page_num,
-                "exceeds vertical margin; expected 9.25 inches of text height between "
-                "margins, found %.2f" % (height / 100),
-            )
+        with Image.open(os.path.join(curdir, fname)) as img:
+            img = img.convert("L")
+            if img.size != (850, 1100):
+                print(
+                    "page size: Page",
+                    page_num,
+                    "is the wrong size; should be 8.5 × 11 inches, found",
+                    img.size[0],
+                    "×",
+                    img.size[1],
+                )
+                continue  # If page is wrong size, nothing else can be checked well
+            if count_nonblank_pixels(img, 0, 0, 65, 1100) > 0:
+                print("margins: Page", page_num, "has content in left margin")
+            if count_nonblank_pixels(img, 785, 0, 850, 1100) > 0:
+                print("margins: Page", page_num, "has content in right margin")
+            if count_nonblank_pixels(img, 0, 0, 850, 75) > 0:
+                print("margins: Page", page_num, "has content in top margin")
+            if count_nonblank_pixels(img, 0, 1025, 850, 1100) > 0:
+                print("margins: Page", page_num, "has content in bottom margin")
 
-# Now check the size of the pages without cropping
-retcode = shared.exec_grouping_subprocesses(
-    "convert -density 100 " + os.path.basename(args.pdf_path) + " nocrop-%d.png",
-    shell=True,
-    cwd=os.path.dirname(args.pdf_path),
-)
-if retcode != 0:
-    exit(retcode)
-
-for fname in os.listdir(os.path.dirname(args.pdf_path)):
-    if fname.startswith("nocrop-"):
-        width, height = get_image_size(fname)
-        page_num = int(fname.split("-")[1].split(".")[0]) + 1
-        if width != 850 or height != 1100:
-            print(
-                "page size: Page",
-                page_num,
-                "is the wrong size; expected 8.5 × 11 inches, found",
-                width / 100,
-                "×",
-                height / 100,
-                "inches",
-            )
+# Check the copyright block on the first page; we can crop some areas in the block that
+# *should* be totally blank (cropping around instructions text) and check that they are
